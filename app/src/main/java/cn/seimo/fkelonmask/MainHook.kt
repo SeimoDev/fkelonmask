4d5912b4783a5af0da580f2986f60e496e6e3441
seimodev <2789263103@qq.com>
2023-07-25 12:33:41 +0800

    初步完成功能实现，等待马斯克修改APP

--- app/src/main/java/cn/seimo/fkelonmask/MainHook.kt
+++ app/src/main/java/cn/seimo/fkelonmask/MainHook.kt
@@ -1,17 +1,95 @@
 package cn.seimo.fkelonmask
 
+import android.content.res.XResources
+import android.graphics.drawable.Drawable
 import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.IXposedHookZygoteInit
+import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
-class MainHook : IXposedHookLoadPackage {
-    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        // 过滤不必要的应用
-        if (lpparam.packageName != "cn.seimo.fkelonmask") return
-        // 执行Hook
-        hook(lpparam)
+class MainHook : IXposedHookZygoteInit, IXposedHookLoadPackage {
+    override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
+        // 初始化资源替换
+        XResources.setSystemWideReplacement("com.twitter.android", "drawable", "twitter_logo", object : XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                // 替换图标资源
+                val res: XResources = param.thisObject as XResources
+                val resName: String = param.args[0] as String
+                val resId: Int = res.getIdentifier(resName, "drawable", "com.twitter.android")
+
+                // 在这里替换为你想要的图片资源
+                val newDrawable: Drawable? = res.getDrawable(R.drawable.twitter_logo)
+
+                if (newDrawable != null) {
+                    param.result = newDrawable
+                }
+            }
+        })
+        XResources.setSystemWideReplacement("com.twitter.android", "drawable", "twitter_logo_white", object : XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                // 替换图标资源
+                val res: XResources = param.thisObject as XResources
+                val resName: String = param.args[0] as String
+                val resId: Int = res.getIdentifier(resName, "drawable", "com.twitter.android")
+
+                // 在这里替换为你想要的图片资源
+                val newDrawable: Drawable? = res.getDrawable(R.drawable.twitter_logo_white)
+
+                if (newDrawable != null) {
+                    param.result = newDrawable
+                }
+            }
+        })
+        XResources.setSystemWideReplacement("com.twitter.android", "mipmap", "ic.launcher_twitter", object : XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                // 替换图标资源
+                val res: XResources = param.thisObject as XResources
+                val resName: String = param.args[0] as String
+                val resId: Int = res.getIdentifier(resName, "mipmap", "com.twitter.android")
+
+                // 在这里替换为你想要的图片资源
+                val newDrawable: Drawable? = res.getDrawable(R.mipmap.ic_launcher_twitter)
+
+                if (newDrawable != null) {
+                    param.result = newDrawable
+                }
+            }
+        })
+        XResources.setSystemWideReplacement("com.twitter.android", "mipmap", "ic_launcher_twitter_foreground", object : XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                // 替换图标资源
+                val res: XResources = param.thisObject as XResources
+                val resName: String = param.args[0] as String
+                val resId: Int = res.getIdentifier(resName, "mipmap", "com.twitter.android")
+
+                // 在这里替换为你想要的图片资源
+                val newDrawable: Drawable? = res.getDrawable(R.mipmap.ic_launcher_twitter_foreground)
+
+                if (newDrawable != null) {
+                    param.result = newDrawable
+                }
+            }
+        })
+        XResources.setSystemWideReplacement("com.twitter.android", "mipmap", "ic_launcher_twitter_round", object : XC_MethodHook() {
+            override fun afterHookedMethod(param: MethodHookParam) {
+                // 替换图标资源
+                val res: XResources = param.thisObject as XResources
+                val resName: String = param.args[0] as String
+                val resId: Int = res.getIdentifier(resName, "mipmap", "com.twitter.android")
+
+                // 在这里替换为你想要的图片资源
+                val newDrawable: Drawable? = res.getDrawable(R.mipmap.ic_launcher_twitter_round)
+
+                if (newDrawable != null) {
+                    param.result = newDrawable
+                }
+            }
+        })
+
+
     }
 
-    private fun hook(lpparam: XC_LoadPackage.LoadPackageParam) {
-        // 具体流程
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
+        TODO("Not yet implemented")
     }
 }
